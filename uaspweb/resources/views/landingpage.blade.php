2fdfbc0ff25d80b4d915c3e190017417f6ba898b
Ardian1408 <ardi3665@gmail.com>
2023-06-18 10:56:37 +0700

    init ubah lokasi

--- uaspweb/resources/views/landingpage.blade.php
+++ uaspweb/resources/views/landingpage.blade.php
@@ -96,7 +96,7 @@
                 <div class="row">
                     <!-- Footer Location-->
                     <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <h4 class="text-uppercase mb-4">Lokasi</h4>
                         <p class="lead mb-0">
                             2215 John Daniel Drive
                             <br />
